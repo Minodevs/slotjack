86d52e8714418082beb4bc9157056138b7ccf8c2
Sezer <bigname32@yahoo.com>
2025-04-13 02:39:27 +0100

    Fix: Complete rebuild of Next.js configuration and dependencies

--- next.config.js
+++ next.config.js
@@ -1,10 +1,1 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  images: {
-    domains: ['picsum.photos'],
-  },
-  output: 'standalone',
-  reactStrictMode: true,
-};
-
-module.exports = nextConfig; 
+ 
